0e15b86d96402dee5e66a27fe9bec6ad48c7c02d
Javier Esteban <javsotos@gmail.com>
2021-04-29 22:22:54 -0400

    feat (nvim): add system verilog language server

--- config/nvim/lua/config/lsp.lua
+++ config/nvim/lua/config/lsp.lua
@@ -1,4 +1,5 @@
 require'lspconfig'.clangd.setup {}
+require'lspconfig'.svls.setup{}
 
 require'nvim-treesitter.configs'.setup {
   ensure_installed = {"c", "bash", "cpp", "css", "html", "python", "regex", "lua"}, 
